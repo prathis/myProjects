69a859fdf18fb47ad0ed4ef5f341f906348c02f7
prathis <prathisravi@hotmail.co.uk>
2021-04-05 00:35:29 +0100

    Add files to the python project folder

---
+++ pythonProjects/Project 2 - Timer.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
+import time
+import datetime
+
+global entTime
+
+def quit(*args):
+    root.destroy()
+    
+def cant_wait():
+    timeLeft = endTime - datetime.datetime.now()
+    timeLeft = timeLeft - datetime.timedelta(microseconds=timeLeft.microseconds)
+
+    txt.set(timeLeft)
+
+    root.after(1000,cant_wait)
+    
+root = Tk()
+root.attributes("-fullscreen", False)
+root.configure(background="black")
+root.bind("x", quit)
+root.after(1000, cant_wait)
+
+endTime = datetime.datetime(2021, 4, 30, 18, 0, 0)
+
+fnt = font.Font(family="Verdana", size=60, weight="bold")
+txt = StringVar()
+lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="white", background="black")
+lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+root.mainloop()
---
+++ pythonProjects/Project 3 - Calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class Application(Frame):
+
+    def _init_(self,master)
+
+        super(application,self)._init_(master)
+        self.task""
+        self.UserIn = StringVar()
+        self.grid()
+        self.create_widgets()
